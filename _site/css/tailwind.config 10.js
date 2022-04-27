e8c8f46e1ded0e159c69c5241a70128e56651d73
Montique Stevens <mstevens528@gmail.com>
2022-04-27 15:26:48 -0400

    misc

---
+++ _site/css/tailwind.config 10.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    plugins: [
+        require('daisyui'),
+    ],
+    daisyui: {
+        styled: true,
+        themes: true,
+        base: true,
+        utils: true,
+        logs: true,
+        rtl: false,
+      },
+  
+}
