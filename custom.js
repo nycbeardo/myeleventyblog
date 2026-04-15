109db81141c67ef4bdcd7d7725454f80867c9d00
Montique Stevens <mstevens528@gmail.com>
2026-04-14 22:07:55 -0400

    Misc

--- custom.js
+++
@@ -1,9 +0,0 @@
-grecaptcha.ready(function() {
-       grecaptcha.execute("6LdyahQdAAAAABcDYhQQpvwufOUoHlGSYVGxOikP", {action: 'homepage'})
-       .then(function(token) {
-         document.getElementById('captchaResponse').value = token;
-         
-       });
-       
-     });
-     
